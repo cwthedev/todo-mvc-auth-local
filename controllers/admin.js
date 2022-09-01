8e34be26bfe60555a170500492b640ab4e7be872
cw100dev <toadix@msn.com>
2022-08-31 21:25:58 -0700

    modified the admin controller and admin panel template so that it displays all users in alpha order and their todos under them

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Todo = require('../models/Todo')
+const User = require('../models/User')
 
 //fetches admin panel page if user has admin status
 module.exports = {
@@ -6,9 +7,10 @@ module.exports = {
         console.log(req.user.isAdmin)
         try{
             if (req.user.isAdmin == true) {
-                const todoItems = await Todo.find({userId:req.user.id})
+                const todoItems = await Todo.find().sort({'userId':1})
+                const userList = await User.find().sort({'userName':1})
                 const itemsLeft = await Todo.countDocuments({userId:req.user.id,completed: false})
-                res.render('adminpanel.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
+                res.render('adminpanel.ejs', {todos: todoItems, left: itemsLeft, user: req.user, users: userList})
             } else if (req.user.isAdmin == false) {
                 res.render('index.ejs')
             }
