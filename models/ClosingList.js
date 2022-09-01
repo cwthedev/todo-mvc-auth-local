bc8a5b5afe85829f2e2d424027540b3e49664b8c
Andrea Watson <awatson.e@gmail.com>
2022-09-01 18:02:54 -0400

    removed commented out sections

--- models/ClosingList.js
+++ models/ClosingList.js
@@ -3,42 +3,20 @@ const mongoose = require('mongoose')
 const ClosingSchema = new mongoose.Schema({
   cleanBathroom: {
     type: String, 
-    // default: false,
-
   },
   getCarts: {
     type: String, 
-    // default: false,
-
   },
   sweep: {
     type: String, 
-    // default: false,
-
   },
   closeRegister: {
     type: String, 
-    // default: false,
-
   },
   userId: {
     type: String,
     required: true
-  }
-
-
-  // todo: {
-  //   type: String,
-  //   required: true,
-  // },
-  // completed: {
-  //   type: Boolean,
-  //   required: true,
-  // },
-  // userId: {
-  //   type: String,
-  //   required: true
-  // }
+  },
 })
 
 module.exports = mongoose.model('ClosingList', ClosingSchema)
--- models/Todo.js
+++ models/Todo.js
@@ -1,43 +1,18 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const TodoSchema = new mongoose.Schema({
-  cleanBathroom: {
-    type: String, 
+// const TodoSchema = new mongoose.Schema({
+//   // todo: {
+//   //   type: String,
+//   //   required: true,
+//   // },
+//   // completed: {
+//   //   type: Boolean,
+//   //   required: true,
+//   // },
+//   // userId: {
+//   //   type: String,
+//   //   required: true
+//   // }
+// })
 
-
-  },
-  getCarts: {
-    type: String, 
-
-
-  },
-  sweep: {
-    type: String, 
-
-
-  },
-  closeRegister: {
-    type: String, 
-
-
-  },
-    userId: {
-    type: String,
-    required: true
-  }
-
-  // todo: {
-  //   type: String,
-  //   required: true,
-  // },
-  // completed: {
-  //   type: Boolean,
-  //   required: true,
-  // },
-  // userId: {
-  //   type: String,
-  //   required: true
-  // }
-})
-
-module.exports = mongoose.model('Todo', TodoSchema)
+// module.exports = mongoose.model('Todo', TodoSchema)
