7a14789f5013f470a0fb90d55b94db1abe54bde8
cw100dev <toadix@msn.com>
2022-09-05 14:07:00 -0700

    modified admin controller to pull 5 days instead of 2.

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,15 +8,15 @@ module.exports = {
     getAdminPanel: async (req,res)=>{
         console.log(req.user.userName)
         try{
-            let twoDaysAgo = new Date();
-            twoDaysAgo.setUTCDate(twoDaysAgo.getDate()-2);
-            twoDaysAgo.setHours(0)
-            twoDaysAgo.setMinutes(0)
-            twoDaysAgo.setSeconds(0)
-            twoDaysAgo.setMilliseconds(0)
-            console.log(twoDaysAgo)
+            let fiveDaysAgo = new Date();
+            fiveDaysAgo.setUTCDate(fiveDaysAgo.getDate()-5);
+            fiveDaysAgo.setHours(0)
+            fiveDaysAgo.setMinutes(0)
+            fiveDaysAgo.setSeconds(0)
+            fiveDaysAgo.setMilliseconds(0)
+            console.log(fiveDaysAgo)
             if (req.user.isAdmin == true) {
-                const closingLists = await ClosingList.find({createdAt: {$gt: twoDaysAgo}}).sort({'createdAt':-1})
+                const closingLists = await ClosingList.find({createdAt: {$gt: fiveDaysAgo}}).sort({'createdAt':-1})
                 const userList = await User.find().sort({'userName':1})
                 res.render('adminpanel.ejs', { lists: closingLists, user: req.user, users: userList}
                 )
