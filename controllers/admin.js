acbc355b0e6e4fbdaf93fae939462d189eddf70c
cw100dev <toadix@msn.com>
2022-09-15 16:57:45 -0700

    added date selector fields to admin panel

--- controllers/admin.js
+++ controllers/admin.js
@@ -6,19 +6,32 @@ const ClosingList = require('../models/ClosingList')
 //fetches admin panel page if user has admin status
 module.exports = {
     getAdminPanel: async (req,res)=>{
-        console.log(req.user.userName)
+        let sDay = new Date(req.query.startDate);
+        let eDay = new Date(req.query.endDate);  
         try{
-            let fiveDaysAgo = new Date();
-            fiveDaysAgo.setUTCDate(fiveDaysAgo.getDate()-5);
-            fiveDaysAgo.setHours(0)
-            fiveDaysAgo.setMinutes(0)
-            fiveDaysAgo.setSeconds(0)
-            fiveDaysAgo.setMilliseconds(0)
-            console.log(fiveDaysAgo)
+            if (sDay == 'Invalid Date' || eDay == 'Invalid Date'){
+                console.log('the if')
+                sDay = new Date();
+                eDay = new Date();
+                sDay.setDate(sDay.getDate()-5);
+                sDay.setHours(0)
+                sDay.setMinutes(0)
+                sDay.setSeconds(0)
+                sDay.setMilliseconds(0)
+            }
+            sDay.setUTCHours(0)
+            sDay.setUTCMinutes(0)
+            sDay.setUTCSeconds(0)
+            sDay.setUTCMilliseconds(0)
+            eDay.setUTCHours(23)
+            eDay.setUTCMinutes(59)
+            eDay.setUTCSeconds(59)
+            eDay.setUTCMilliseconds(999)
+
             if (req.user.isAdmin == true) {
-                const closingLists = await ClosingList.find({createdAt: {$gt: fiveDaysAgo}}).sort({'createdAt':-1})
+                const closingLists = await ClosingList.find({createdAt: {$gte: sDay, $lte: eDay}}).sort({'createdAt':-1})
                 const userList = await User.find().sort({'userName':1})
-                res.render('adminpanel.ejs', { lists: closingLists, user: req.user, users: userList}
+                res.render('adminpanel.ejs', { lists: closingLists, user: req.user, users: userList, sDay: sDay, eDay: eDay}
                 )
             } else if (req.user.isAdmin == false) {
                 res.render('index.ejs')
